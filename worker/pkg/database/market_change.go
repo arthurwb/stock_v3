8b1aaa54271a4e0297a94299b8aa2c27c2ce288a
Brooks <brooksarthur07@gmail.com>
2025-05-07 21:14:18 +0000

    switch to unix time

--- worker/pkg/database/market_change.go
+++ worker/pkg/database/market_change.go
@@ -7,10 +7,31 @@ import (
 )
 
 func MarketChange(tx *sql.Tx, eqEffects string, eqStartDate string) (bool) {
-	timeNow := time.Now().Truncate(time.Minute).Format("2006-01-02T15:04:05Z")
-	log.Println(timeNow)
-	log.Println(eqStartDate)
-	if (timeNow == eqStartDate) {
+	// Parse eqStartDate - try multiple formats to handle both cases
+    var startDate time.Time
+    var err error
+    
+    // Try ISO 8601 format first (with T and Z)
+    startDate, err = time.Parse(time.RFC3339, eqStartDate)
+    if err != nil {
+        // Try space-separated format with milliseconds
+        startDate, err = time.Parse("2006-01-02 15:04:05.000", eqStartDate)
+        if err != nil {
+            log.Println("Error parsing date:", err)
+            return false
+        }
+    }
+    
+    // Get current time and truncate to minutes
+    currentTime := time.Now().Truncate(time.Minute)
+    
+    // Convert both to Unix time (seconds since epoch)
+    currentUnix := currentTime.Unix()
+    startUnix := startDate.Unix()
+    
+    log.Println("Current Unix time:", currentUnix)
+    log.Println("Start date Unix time:", startUnix)
+	if (currentUnix == startUnix) {
 		switch eqEffects {
 			case "bull": UpdateMarket(tx, "bull")
 			case "bear": UpdateMarket(tx, "bear")
